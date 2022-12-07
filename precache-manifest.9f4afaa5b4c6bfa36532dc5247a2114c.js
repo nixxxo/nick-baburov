8a3f5641f35c31774e5553ae6cec6a642735d1e1
nixxxo <xXinfinitescopeXx@gmail.com>
2022-12-07 12:26:52 +0200

    Updates

---
+++ precache-manifest.9f4afaa5b4c6bfa36532dc5247a2114c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9339ff5395a11139473a",
+    "url": "/portfolio/static/css/main.03b86263.chunk.css"
+  },
+  {
+    "revision": "9339ff5395a11139473a",
+    "url": "/portfolio/static/js/main.9339ff53.chunk.js"
+  },
+  {
+    "revision": "a1d82fca01947fc2e2e9",
+    "url": "/portfolio/static/js/1.a1d82fca.chunk.js"
+  },
+  {
+    "revision": "ae3d2eede3359cac271a",
+    "url": "/portfolio/static/js/2.ae3d2eed.chunk.js"
+  },
+  {
+    "revision": "86833aeaebdf5219441e",
+    "url": "/portfolio/static/js/runtime~main.86833aea.js"
+  },
+  {
+    "revision": "9224eb768ad971c19bf5827769efc110",
+    "url": "/portfolio/static/media/neology_deco_w03_bold-webfont.9224eb76.woff2"
+  },
+  {
+    "revision": "49ff30cefb0eeff5be84910172aa245c",
+    "url": "/portfolio/static/media/neology_deco_w03_bold-webfont.49ff30ce.woff"
+  },
+  {
+    "revision": "291a7b7ea79beead66d5e86243f01bad",
+    "url": "/portfolio/static/media/Full-Logo.291a7b7e.png"
+  },
+  {
+    "revision": "37b5dd17966db0eaa02d92b6a37b3522",
+    "url": "/portfolio/index.html"
+  }
+];
